812fa99081f81dbb2913a72240672eb6b38e47e5
Ernst Oldenhof <ernst.oldenhof@gmail.com>
2025-08-31 12:02:10 +0200

    restructuring: define individual agents in agents/, import these in main.py

---
+++ agents/data_describer.py
@@ -0,0 +1,78 @@
+import os
+from typing import List
+from dotenv import load_dotenv
+import pathlib
+from pydantic import BaseModel, Field
+import pandas as pd
+from src.agent.environment import Environment
+from src.agent.actions import ActionRegistry, Action
+from src.agent.actions import list_files, list_column_names_of_dataframe, describe_column, describe_dataframe, show_datatype_of_column
+from src.agent.actions import ListFilesParams, ListColumnNamesOfDataFrameParams, DescribeColumnParams, DescribeDataframeParams, ShowDatatypeOfColumnParams
+from src.agent.agent import Agent, AgentLanguage, AgentFunctionCallingActionLanguage, generate_response
+from src.agent.goals import Goal
+load_dotenv() # This loads variables from .env into os.environ
+
+goals = [
+    Goal(priority=1, name="Gather Information", description="Giving a summary of all data present in the data directory, "\
+    "by listing all files in the directory, and describing the dataframes. "),
+    Goal(priority=1, name="Terminate", description="Call the terminate call when you have descriptions of all dataframes, "\
+        "or when there is an indication that the file loading is unsuccessful or you run into other problems.")
+]
+
+# Create and populate the action registry
+action_registry = ActionRegistry()
+
+action_registry.register(Action(
+    name="list_files",
+    function=list_files,
+    description="List all files in the current directory",
+    pydantic_base_model=ListFilesParams,
+    terminal=False
+))
+
+action_registry.register(Action(
+    name="list_column_names_of_dataframe",
+    function=list_column_names_of_dataframe,
+    description="List the columns of the dataframe",
+    pydantic_base_model=ListColumnNamesOfDataFrameParams,
+    terminal=False
+))
+
+action_registry.register(Action(
+    name="describe_dataframe",
+    function=describe_dataframe,
+    description="Describe the dataframe",
+    pydantic_base_model=DescribeDataframeParams,
+    terminal=False
+))
+
+action_registry.register(Action(
+    name="show_datatype_of_column",
+    function=show_datatype_of_column,
+    description="Show the datatypes of a particular column. ",
+    pydantic_base_model=ShowDatatypeOfColumnParams,
+    terminal=False
+))
+
+action_registry.register(Action(
+    name="describe_column",
+    function=describe_column,
+    description="Describe a particular column in the dataframe",
+    pydantic_base_model=DescribeColumnParams,
+    terminal=False
+))
+
+
+# Define the environment
+environment = Environment()
+agent_language = AgentFunctionCallingActionLanguage()
+
+describe_agent = Agent(goals, agent_language, action_registry, generate_response, environment)
+
+if __name__ == '__main__':
+    # Run the agent with user input
+    user_input = "Describe the data in this directory."
+    final_memory = describe_agent.run(user_input)
+
+    # Print the final memory
+    print(final_memory.get_memories())
--- src/agent/actions.py
+++ src/agent/actions.py
@@ -1,6 +1,7 @@
 from typing import List, Callable, Dict, Any, Union
 import os
 import pandas as pd
+import pathlib
 from langchain_core.utils.function_calling import convert_to_openai_function
 from pydantic import BaseModel, Field
 
@@ -51,4 +52,67 @@ class ActionRegistry:
         return list(self.actions.values())
 
 
+## Actions
+data_dir = "data/"
+
+def load_df_from_path(path: str):
+    full_path = pathlib.Path(data_dir) / path
+    if not os.path.exists(full_path):
+        raise ValueError(f"File not found at path: {full_path}")
+    return pd.read_csv(full_path)
+
+# Function definitions. These can be called by the LLM.
+def list_files() -> list:
+    """List all files in the data directory."""
+    return os.listdir(data_dir)
+
+def list_column_names_of_dataframe(path: str) -> List[str]:
+    """List column names of a pandas DataFrame."""
+    df = load_df_from_path(path)
+    return list(df.columns)
+
+def describe_dataframe(path: str) -> str:
+    """Describe the contents of a pandas DataFrame."""
+    df = load_df_from_path(path)
+    return df.describe().to_string()
+
+def show_datatype_of_column(path: str, column_name: str) -> str:
+    """Show the datatype of a column in a pandas DataFrame."""
+    df = load_df_from_path(path)
+    return str(df[column_name].dtype)
+
+def describe_column(path: str, column_name: str) -> str:
+    """Describe the contents of a column in a pandas DataFrame."""
+    df = load_df_from_path(path)
+    return df[column_name].describe().to_string()
+
+def translate_pd_to_human(message) -> None:
+    """ Translate the pandas results into a human-readable text.
+    This will terminate the loop.
+    """
+    print(f"The pandas results can be described as follows: {message}")
+
+
+# Parameter definitions in Pydantic.
+class ListFilesParams(BaseModel):
+    """List files in the data directory"""
+    pass
+
+class ListColumnNamesOfDataFrameParams(BaseModel):
+    """List column names of a pandas DataFrame."""
+    path: str = Field(..., description="The path to the dataframe to read from")
+
+class DescribeDataframeParams(BaseModel):
+    """Describe the contents of the dataframe"""
+    path: str = Field(..., description="The path to the dataframe to read from")
+
+class ShowDatatypeOfColumnParams(BaseModel):
+    """Show the datatype of a column in a pandas DataFrame."""
+    path: str = Field(..., description="The path to the dataframe to read from")
+    column_name: str = Field(..., description="The name of the column to show the datatype of.")
+
+class DescribeColumnParams(BaseModel):
+    """Describe the contents of a column in a pandas DataFrame."""
+    path: str = Field(..., description="The path to the dataframe to read from")
+    column_name: str = Field(..., description="The name of the column to describe.")
 
